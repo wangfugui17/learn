327a813cbcb2e9d4f72f27114f0d3bfd858abf6a
wangjiancheng <wangjiancheng@thewesthill.net>
2024-08-02 16:06:31 +0800

    修改了微信自动回复

--- 八月的内容/2024_8_1/这是微信自动回复ai.py
+++ 八月的内容/2024_8_1/这是微信自动回复ai.py
@@ -1,98 +1,78 @@
-# coding: utf-8
-#
+
 import pandas as pd
 from IPython.display import display,clear_output,Markdown
 import os
 from coze import Coze
+import uiautomator2 as u2
+import time
+
 os.environ['COZE_API_TOKEN'] = 'pat_ESCxaeS8auGTdt3GG1CwCMJgkgTTJoVEqCh048coFSjjUjkKNTPsPo0zfnA8NdtK'
 os.environ['COZE_BOT_ID'] = "7397332462814494760"
-
-
-
 
 chat = Coze(api_token= os.environ['COZE_API_TOKEN'],
             bot_id = os.environ['COZE_BOT_ID'],
             max_chat_rounds=10,
             stream=False)
 
-
-
-# 获取当前前台应用的包名
-import uiautomator2 as u2
 d = u2.connect()
-# 获取当前前台应用信息
-current_app = d.app_current()
 
-# 尝试查找具有文本 "微信" 的元素
-wechat_element = d(text="微信")
-
-# 如果找到了，说明应用未打开，启动应用
-if  wechat_element.exists:
-    package_name = "com.tencent.mm"  # 假设微信的包名是这个
-    d.app_start(package_name)
-
-# 找到所有类名为 android.widget.RelativeLayout 的元素
-# relative_layout_elements = d(className="android.widget.RelativeLayout")
-#
-# for layout in relative_layout_elements:
-#     # 遍历 RelativeLayout 的子元素
-#     children = layout.child().child(resourceId = 'com.tencent.mm:id/o4k').child(className = '''android.widget.LinearLayout
-# ''').child()
-#     print(children.get_text())
-#     for child in children:
-#         if child.get_text() == 'DROP TABLE user_name':
-#             # 尝试获取 id 为 com.tencent.mm:id/ht5 的子元素的 text
-#             target_child = layout.child(resourceId="com.tencent.mm:id/ht5")
-#             if target_child.exists:
-#                 print(target_child.get_text())
-
-
-# yemian = d(text = 'DROP TABLE user_name').down(className = 'android.view.View').get_text()
-# print(yemian)
-# user_message ={}
-#
-# childen = list(d(className ='android.widget.LinearLayout'))
-# print(childen.get_text())
-# for child in childen:
-#     if child.child(resourceId='com.tencent.mm:id/o_u').get_text() is not None:
-#         user_name = d(resourceId='com.tencent.mm:id/o_u').right(className='android.view.View').get_text()
-#         user_message[user_name] = d(resourceId='com.tencent.mm:id/o_u').get_text()
-#         print(user_name)
-#         print(user_message)
-#     else:
-#         print('没有新消息')
-
-
-import uiautomator2 as u2
-import time
-
-d = u2.connect()
 
 fu_jiedian = d(resourceId="com.tencent.mm:id/bp0").child(resourceId='com.tencent.mm:id/bn1')
-new_message = ''
 path = d(resourceId="com.tencent.mm:id/bkl")
+new_message = ''
 flag = False
-def panduan():
-    if path[-1].right(className='android.widget.RelativeLayout') is None:
-        flag = True
 
-def chat_ai_auto():
+def panduan():
+    global path
+    global flag
+    while flag == False:
+        if path[-1].right(className='android.widget.RelativeLayout') is None:
+            flag = True
+            print(flag)
+        time.sleep(1)
+    print(flag)
+
+def chat_ai_auto(*a,**b):
+    global flag
+    global new_message
     panduan()
     for i in range(len(fu_jiedian)):
+        print(i)
+        print(len(fu_jiedian))
         new_message = path[i].get_text()
 
     if flag:  # 判断是不是自己发出去的
         botAi = chat(new_message)
-        # 在这里延迟10秒执行
-        # time.sleep(10)
         d.xpath('//android.widget.EditText').set_text('')
-        input_text = d.xpath('//android.widget.EditText').set_text(f'{botAi}')
+        d.xpath('//android.widget.EditText').set_text(f'{botAi}')
         send_input = d(resourceId="com.tencent.mm:id/bql")
         send_input.click()
+        flag = False
+
     else:
         print('对方还没有回复')
+    # return chat_ai_auto()
+def xiaoxiye():
+    message_list = d(resourceId="com.tencent.mm:id/j8g").child(resourceId = 'com.tencent.mm:id/cj1')
+    message_num = message_list.child(resourceId = 'com.tencent.mm:id/o_u')
 
-    print(f'这是最新的信息:{new_message}')
-chat_ai_auto()
+    if new_message_text() > 0:
+
+        for i in range(1,len(message_list)):
+            if d(resourceId="com.tencent.mm:id/cj0")[i].child(className="android.widget.RelativeLayout").child(resourceId='com.tencent.mm:id/o_u') is not None:
+                message_list[i].child(resourceId='com.tencent.mm:id/a_4').click()
+                chat_ai_auto()
+                d(description='返回').click()
+                print('找到消息了')
+            else:
+                print('暂时没找到新消息')
+
+xiaoxiye()
 
 
+def new_message_text():
+    message_fu_addr = d(resourceId="com.tencent.mm:id/cj0")[0].child(className="android.widget.RelativeLayout").child(resourceId='com.tencent.mm:id/o_u')
+    while len(message_fu_addr) <= 0:
+        print('收到新消息了！')
+    return len(message_fu_addr)
+new_message_text()
